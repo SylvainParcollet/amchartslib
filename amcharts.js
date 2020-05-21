b01e84be9a0d8ebfcf89721a932c2bfcecbcdbe1
SylvainParcollet <64834974+SylvainParcollet@users.noreply.github.com>
2020-05-21 19:36:28 +0200

    Update amcharts.js

--- amcharts.js
+++ amcharts.js
@@ -664,7 +664,7 @@ polygonSeries.events.on("inited", function () {
 					console.log("************Sankey chart************");    
 					Sankeychartkaramba(Ar[0].div,"");
 				}
-				else if
+				elseif
 				{
 					console.log("************Map chart************");    
 					Mapkaramba(Ar[0].div,"");	
